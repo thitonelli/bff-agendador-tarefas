16d8f0e05f658ed8475a92ce037e8fd21c55aab6
Thiago <thifabris@gmail.com>
2026-09-15 15:48:45 -0300

    primeiro commit projeto

---
+++ src/test/java/com/estudo/bffagendadortarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.bffagendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
